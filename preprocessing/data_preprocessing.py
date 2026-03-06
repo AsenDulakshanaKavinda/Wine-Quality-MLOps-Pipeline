537b0debd19bf1826a6fa797c5992ee0211102c9
AsenDulakshanaKavinda <s22010362@ousl.lk>
2026-03-06 21:48:21 +0530

    add data normalization

--- preprocessing/data_preprocessing.py
+++ preprocessing/data_preprocessing.py
@@ -26,4 +26,18 @@ class DataPreprocessing():
         except Exception as e:
             RuntimeError(f"Error while data ingestion, error: {str(e)}") 
 
-    def 
+    def data_normalization(self, df, num_col_list: list[str]):
+        if not df:
+            raise ValueError("Missing dataframe")
+
+        if not num_col_list:
+            raise ValueError("Missing numerical columns list")
+
+        scale = MinMaxScaler()
+
+        try:
+            df[num_col_list] = scale.fit_transform(df[num_col_list])
+            return df
+        except Exception as e:
+            raise RuntimeError(f"Error while normalizing data, error: {str(e)}")
+
